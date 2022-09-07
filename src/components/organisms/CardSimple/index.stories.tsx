002ecb59968305a75076146f936bd14723bd22ed
熊倉 秀人 <h_kumakura@MacBook-Pro.local>
2022-09-07 10:51:39 +0900

    add CardSimple

---
+++ src/components/organisms/CardSimple/index.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, Story } from "@storybook/react/types-6-0";
+import { CardSimple } from "./index";
+
+export default {
+  component: CardSimple,
+  title: "organisms/CardSimple",
+} as Meta;
+
+const props = {
+  imageUrl: "https://www.yumemi.co.jp/images/logo_yumemi_02.svg",
+  text: "テキストテキストテキストテキaストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+};
+
+const Template: Story = () => <CardSimple {...props}/>;
+
+export const cardsimple = Template.bind({});
---
+++ src/components/organisms/CardSimple/index.tsx
@@ -0,0 +1,47 @@
+import { VFC } from 'react';
+import styled from 'styled-components';
+import { colors, breakPoints } from 'src/styles/Tokens';
+
+interface CardSimpleProps {
+  imageUrl: string;
+  text: string;
+}
+
+const Wrapper = styled.section`
+  font-size: 12px;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+  width: 200px;
+  img {
+    width: 200px;
+    border-radius: 8px 8px 0px 0px;
+    height: 200px;
+    background: ${colors.Gray};
+  }
+  p {
+    width: 200px;
+    padding: 8px;
+  }
+  @media screen and (min-width: ${breakPoints.pcDisplaySize}) {
+    width: 244px;
+    img {
+      width: 244px;
+      height: 244px;
+    }
+    p {
+      width: 244px;
+      padding: 8px;
+    }
+  }
+`;
+
+export const CardSimple: VFC<CardSimpleProps> = (props) => {
+  const { imageUrl, text } = props;
+
+  return (
+    <Wrapper>
+      <img src={imageUrl} alt="" />
+      <p>{text}</p>
+    </Wrapper>
+  );
+};
